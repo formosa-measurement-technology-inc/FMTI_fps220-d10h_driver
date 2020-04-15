7c4862dc443e8503d9aa89395f5b2d0f86a3f068
Conrad <conradalf@gmail.com>
2020-04-15 18:31:20 +0800

    1. Fix the bugs that happened as being run on an 8 bits mcu. 2. Add the code snippets for SPI 4 wires mode.

--- fps220_d10h.c
+++ fps220_d10h.c
@@ -195,18 +195,26 @@ int8_t fps220_init(void)
 	int32_t err;
 	uint8_t data_buf;
 
+	fps220_barom.delay_usec = fps220_us_delay;
+	/* The minimum start up time of fps220 is 25ms */
+	barom->delay_usec(1000 * 25);
+
 #ifdef SPI
 	fps220_barom.bus_write = fps220_spi_writeblock;
 	fps220_barom.bus_read = fps220_spi_readblock;
+	/* The default of SPI is in 3 wires mode after power on reset. If 4 wires SPI
+    mode is preffered, the following statements will be needed. */
+    #define SPI_4_WIRES_MODE
+	#ifdef SPI_4_WIRES_MODE
+		/* Set SPI bus as 4 wires mode */
+		data_buf = FPS220_SPI_CTRL_REG_SDO_ACTIVE_EN;
+		barom->bus_write(FPS220_SPI_CTRL_REG, sizeof(uint8_t), &data_buf);
+	#endif
 #else
 	fps220_barom.bus_write = fps220_i2c_writeblock;
 	fps220_barom.bus_read = fps220_i2c_readblock;
 #endif
-	fps220_barom.delay_usec = fps220_us_delay;
-
-	/* The minimum start up time of fps220 is 25ms */
-	barom->delay_usec(1000 * 25);
-
+	
 	err = fps220_chipid_check(barom);
 	if (err) {
 		err = -1;
@@ -350,7 +358,7 @@ static int fps220_get_raw_temperature(struct fps220_data *barom)
 	uint8_t buf[3] = {0};
 
 	err = barom->bus_read(FPS220_READ_MEAS_REG_U, 3 * sizeof(uint8_t), buf);
-	barom->raw_temperature = (buf[0] * 256 * 256) + (buf[1] * 256) + buf[2];
+	barom->raw_temperature = ((uint32_t)buf[0] << 16) + ((uint32_t)buf[1] << 8) + buf[2];
 
 #ifdef DEBUG_FPS220
 	printf("%s: uncompensated temperature: %d\n", DEVICE_NAME, barom->raw_temperature);
@@ -390,9 +398,8 @@ static int32_t fps220_get_raw_pressure(struct fps220_data *barom)
 	uint8_t buf[3] = {0};
 
 	err = barom->bus_read(FPS220_READ_MEAS_REG_U, 3 * sizeof(uint8_t), buf);
-
-	barom->raw_pressure = (buf[0] * 256 * 256) + (buf[1] * 256) + buf[2];
-
+	barom->raw_pressure = ((uint32_t)buf[0] << 16) + ((uint32_t)buf[1] << 8) + buf[2];
+	
 #ifdef DEBUG_FPS220
 	printf("%s: uncompensated pressure:  %d\n", DEVICE_NAME, barom->raw_pressure);
 #endif
